4f472a7033d61aaa8f30ad6ce52d3372f26f0b07
m.koponirov <koponirov92@gmail.com>
2020-05-07 01:06:48 +0300

    Created ability to generate cards into definite elememts, selected by className;

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
+'use strict';
+
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
@@ -18,8 +12,18 @@ const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
 const loginWarning = document.querySelector('.login-warning');
 const passwordWarning = document.querySelector('.password-warning');
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('userLogin');
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
 
 function toggleModalAuth () {
   modalAuth.classList.toggle('is-open');
@@ -50,6 +54,7 @@ function authorized() {
   buttonOut.style.display = 'block';
   buttonOut.addEventListener('click',logOut)
 }
+
 function notAuthorized() {
 
   console.log('не авторизован');
@@ -87,4 +92,91 @@ function checkAuth() {
   }
 }
 
+function createCardRestaurant() {
+  const card = `
+            <a class="card card-restaurant">
+				<img src="img/pizza-burger/preview.jpg" alt="image" class="card-image"/>
+					<div class="card-text">
+						<div class="card-heading">
+							<h3 class="card-title">PizzaBurger</h3>
+							<span class="card-tag tag">45 мин</span>
+					    </div>
+						<div class="card-info">
+							<div class="rating">
+								4.5
+							</div>
+							<div class="price">От 700 ₽</div>
+							<div class="category">Пицца</div>
+						</div>
+					</div>
+			</a>
+  `;
+
+  cardsRestaurants.insertAdjacentHTML("beforeend", card);
+
+}
+
+function openGoods(event) {
+  const target = event.target;
+
+  const restaurant = target.closest('.card-restaurant');
+
+  if (login&&restaurant) {
+    containerPromo.classList.add('hide');
+    restaurants.classList.add('hide');
+    menu.classList.remove('hide');
+
+    cardsMenu.textContent = '';
+
+    createCardGood ();
+    createCardGood ();
+    createCardGood ();
+  } else {
+    toggleModalAuth ()
+  }
+}
+
+function createCardGood () {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  card.insertAdjacentHTML("beforeend",`
+    <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+		<div class="card-text">
+			<div class="card-heading">
+				<h3 class="card-title card-title-reg">Пицца Классика</h3>
+			</div>
+			<div class="card-info">
+				<div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями,
+									грибы.
+				</div>
+			</div>
+			<div class="card-buttons">
+				<button class="button button-primary button-add-cart">
+					<span class="button-card-text">В корзину</span>
+					<span class="button-cart-svg"></span>
+				</button>
+				<strong class="card-price-bold">510 ₽</strong>
+			</div>
+		</div>
+  `);
+
+  cardsMenu.insertAdjacentElement("beforeend", card);
+}
+
+cardsRestaurants.addEventListener('click',openGoods);
+logo.addEventListener('click',function () {
+    containerPromo.classList.remove('hide')
+    restaurants.classList.remove('hide')
+    menu.classList.add('hide')
+})
+
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
 checkAuth();
+
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
